285fa9d96bd00611bfc0ac362112180ddb8af480
James Nope <JamesNope@APPLEs-iMac.local>
2019-02-28 21:57:48 -0500

    Modificacion en los modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Rutas
 import { AppRoutingModule } from './app-routing.module';
+
+// Modulos
+import { PagesModule } from './pages/pages.module';
+
+// Componentes
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { PagesComponent } from './pages/pages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    DashboardComponent,
-    Graficas1Component,
-    HeaderComponent,
-    SidebarComponent,
-    NopagefoundComponent,
-    BreadcrumbsComponent,
-    ProgressComponent,
-    PagesComponent,
     RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
